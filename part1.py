126728b786f5806c16a5d9b62a92a07a3edde5bf
gmarraro <marrarogaby@gmail.com>
2018-02-21 22:12:28 -0500

    Update part1.py

--- part1.py
+++ part1.py
@@ -10,40 +10,30 @@ from collections import Counter
 import codecs
     
 def find_ttr():
-    #Initialize i to loop through list of 1000-word lists
     i=0
-    #Initialize number to find average ttr 
     ttr_mean=0
     with codecs.open('agatha.txt',encoding='utf-8', errors='ignore') as ac:
         tokens = []
-        #Read the text file and split by spaces into words
         text = ac.read().split()
         #Clean each word in the list: remove punctuation, convert to lower case
         for word in text:
             word = "".join(x for x in word if x.isalnum()).lower()
-            #Append each formatted word to a list of tokens
             tokens.append(word)
-        #Create a list of lists based on length of book divided by 1000
         tokens_list=[[] for i in range((len(tokens)//1000)+1)]
-        #Beginning with the first list in tokens_list
         for word in tokens:
-            #Append word to sub-list until it contains 1000 words
+            #Create series of 1000-word lists
             if len(tokens_list[i])< 1001:
                 tokens_list[i].append(word)
-            #When list has 1000 items, increase i to fill next sub-list
             if len(tokens_list[i]) ==1000:
                 i+=1
-        #For each 1000-word chunk
         for item in tokens_list:
             #Use Counter function to count instances of words in each list
             types = Counter(item)
             #Find ttr 
             ttr = len(types)/len(item)
-            #Add ttr to mean to use to calculate average over the 75 lists 
+            #Calculate average over the 75 lists 
             ttr_mean=ttr_mean+ttr
-        #Calculate average mean over the 75 lists
         ttr_mean=ttr_mean/len(tokens_list)
         return ttr_mean
 
 find_ttr()
-#TTR = 0.4389
